34db135c0b1e234b7354315e175d77c227d90f90
BernardoVxexra <bernardo.santos13@etec.sp.gov.br>
2025-10-14 10:09:00 -0300

    Carregando o tcc nas costas

--- models/ContactModel.js
+++ models/ContactModel.js
@@ -1,30 +1,30 @@
-import { getDbConnection } from './database/db.js';
-
+import { getDbConnection } from '../database/db.js';
 export class ContactModel {
     
-    static async createContact(nome, telefone, email){
-        const db = await getDbConnection();
-        await db.run(
-            'INSERT INTO Contato (nome, telefone, email) VALUES (? , ? , ?)' , 
-            [nome, telefone, email]
-        );
-    }
+    //Criar contato 
+   static async createContact({nome, telefone, email}){
+    const db = getDbConnection();
+    await db.run(
+        'INSERT INTO Contato (nome, telefone, email) VALUES (?,?,?)',
+        [nome, telefone, email]
+    );
+   }
 
-    static async findByName(nome){
-        const db = await getDbConnection();
-        return db.get('SELECT * FROM Contato WHERE nome = ?', [nome])
-    }
+    static async findById(id_contato) {
+    const db = await getDbConnection();
+    const user = await db.get('SELECT * FROM Contato WHERE id_contato = ?' , [id_contato]);
+    return user;
+  }
 
-    static async updateByContact(id_contato, nome, telefone, email){
-        const db = await getDbConnection();
-        await db.run(
-            'UPDATE Contato SET nome = ?, telefone = ?, email = ? WHERE id_contato = ?',
-            [nome, telefone , email, id_contato]
-        )
-    }
-
-    static async deleteByContact(id_contato){
-        const db = await getDbConnection();
-        await db.run('DELETE FROM Contato WHERE id_contato = ?', [id_contato]);
-    }
+  static async updateContact(id_contato, {nome, telefone, email}){
+    const db = getDbConnection;
+    const result = await db.run('UPDATE Contato SET nome = ?, telefone = ? WHERE id_contato = ?', [nome, telefone, email, id_contato])
+    return result;  
+  }
+  
+  static async deleteContact(id_contato){
+    const db = getDbConnection();
+    const user = await db.run('DELETE FROM Contato WHERE id_contato = ? ', [id_contato])
+    return result;
+  }
 }
