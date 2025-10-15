2aa70a39061ffcff4567ee9f98a2dca2359ba0fb
BernardoVxexra <bernardo.santos13@etec.sp.gov.br>
2025-10-15 19:43:03 -0300

    Progresso

---
+++ controllers/IoTController.js
@@ -0,0 +1,15 @@
+import { IoTModel} from '../models/IoTModel.js';
+
+export class IoTController {
+    // Criar função receiveData para receber dados do dispositivo IoT
+
+    // 1 - Receber dados do dispositivo IoT / Salvar leitura
+
+    // 2 - Checar se o valor recebido está dentro do esperado/critico
+
+    // 3- Se estiver em estado critico, buscar contatos
+
+    // 4 - Criar alerta
+
+    // 5- Enviar SMS
+}
---
+++ models/IoTModel.js
@@ -0,0 +1,11 @@
+import { getDbConnection }  from "../database/db";
+
+// create Leitura (id_dispositivo, distancia, impacto, movimentacao, acidente_identificado)
+// getClienteByDispositivo(id_dispositivo)
+
+// Alerta 
+// create({ descricao, codigo, id_contato })
+
+// Serviços
+// sendSMS(telefone, mensagem)
+// sendPushNotification(pushToken, mensagem)
